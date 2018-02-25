b1bd7268c2c399a48d7f55fd546601b83c78eee7
John McBride <jpmmcbride@gmail.com>
2018-02-24 22:30:09 -0700

    Working on the server

--- server.js
+++ server.js
@@ -24,16 +24,57 @@ app.use(bodyParser.json());
 // ---------------
 // Get the NEON-JS modules
 // ---------------
-var neon_js = require('@cityofzion/neon-js')
+var neon_js = require('@cityofzion/neon-js');
 
 // Semantic Style by using default import
-var Neon = neon_js.default
-const query = Neon.create.query()
+var Neon = neon_js.default;
+const query = Neon.create.query();
 
 // Named imports are available too
-var wallet = neon_js.wallet
-var tx = neon_js.tx
+var wallet = neon_js.wallet;
+var tx = neon_js.tx;
 
+//console.log("------\nThe Wallet object\n---------");
+//console.log(wallet);
+
+const privateKey = wallet.generatePrivateKey(); 
+// console.log("------\nPrivate key\n---------");
+// console.log(privateKey);
+
+const publicKey = wallet.getPublicKeyFromPrivateKey(privateKey);
+// console.log("------\n The public key\n---------");
+// console.log(publicKey);
+
+const scriptHash = wallet.getScriptHashFromPublicKey(publicKey); 
+// console.log("------\nscript Hash\n---------");
+// console.log(scriptHash);
+
+const address = wallet.getAddressFromScriptHash(scriptHash);
+// console.log("------\nThe address\n---------");
+// console.log(address);
+
+console.log(address);
+
+var balance;
+
+// Get the balance method
+neon_js.api.neoscan.getBalance('http://localhost:4000/api/main_net', address)
+	.then(function(res){
+		console.log(res);
+		balance = res;
+	}).catch(function(err){
+		console.log(err);
+	});
+
+neon_js.
+// neon_js.api.sendAsset({ 
+// 	"balance": balance, // Need this?
+// 	"privateKey": "KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr",
+// 	"intents": neon_js.api.makeIntent( {"NEO": 1}, address ),
+// 	"url": "localhost:30333" // Change to google server
+// }).then(function(res){
+// 	console.log(res);
+// });
 /*
 const account = new wallet.Account(privateKey)
 
